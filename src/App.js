5ff1e515b8d0e74a4f6920b1c04d57d8dca77d19
Vahrushev Anton <toha.vahrushev2010@yandex.ru>
2019-09-26 18:13:40 +0300

    remove data from App.js

--- src/App.js
+++ src/App.js
@@ -1,42 +1,7 @@
 import React from 'react';
 import Result from './components/Results/Results';
 import Test from './components/Test/Test';
-
-const questions = [
-	{
-		id: '1',
-		question: 'Выбери правильный ответ:',
-		answers: [
-			{ text: 'Первый вариант', id: '1' },
-			{ text: 'Второй вариант', id: '2' },
-			{ text: 'Третий вариант', id: '3' },
-			{ text: 'Четвёртый вариант', id: '4' },
-		],
-		right: '1',
-	},
-	{
-		id: '2',
-		question: 'Выбери правильный ответ 2:',
-		answers: [
-			{ text: 'Первый вариант', id: '1' },
-			{ text: 'Второй вариант', id: '2' },
-			{ text: 'Третий вариант', id: '3' },
-			{ text: 'Четвёртый вариант', id: '4' },
-		],
-		right: '2',
-	},
-	{
-		id: '3',
-		question: 'Выбери правильный ответ 3:',
-		answers: [
-			{ text: 'Первый вариант', id: '1' },
-			{ text: 'Второй вариант', id: '2' },
-			{ text: 'Третий вариант', id: '3' },
-			{ text: 'Четвёртый вариант', id: '4' },
-		],
-		right: '3',
-	},
-];
+import questions from './data';
 
 class App extends React.Component {
 	state = {

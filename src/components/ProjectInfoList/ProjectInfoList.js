24aca65951cb7d42e50769ae171f41aa4fe32bf3
Vahrushev Anton <toha.vahrushev2010@yandex.ru>
2019-10-08 14:49:04 +0500

    project info

--- src/components/ProjectInfoList/ProjectInfoList.js
+++ src/components/ProjectInfoList/ProjectInfoList.js
@@ -6,11 +6,19 @@ function ProjectInfoList() {
 		<ul className="ProjectInfoList">
 			<li className="ProjectInfoList__item">
 				<span className="ProjectInfoList__item-title">React</span>
-				<span className="ProjectInfoList__item-version">16.3.0</span>
+				<span className="ProjectInfoList__item-version">16.9.0</span>
 			</li>
 			<li className="ProjectInfoList__item">
-				<span className="ProjectInfoList__item-title">React</span>
-				<span className="ProjectInfoList__item-version">16.3.0</span>
+				<span className="ProjectInfoList__item-title">ReactDOM</span>
+				<span className="ProjectInfoList__item-version">16.9.0</span>
+			</li>
+			<li className="ProjectInfoList__item">
+				<span className="ProjectInfoList__item-title">Create React App</span>
+				<span className="ProjectInfoList__item-version">3.1.2</span>
+			</li>
+			<li className="ProjectInfoList__item">
+				<span className="ProjectInfoList__item-title">PropTypes</span>
+				<span className="ProjectInfoList__item-version">15.7.2</span>
 			</li>
 		</ul>
 	);

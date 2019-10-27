d23fec7768e2c782841764f511ad2e8b22f7c186
Vahrushev Anton <toha.vahrushev2010@yandex.ru>
2019-10-27 15:25:09 +0300

    redux devtools hotfix

--- src/store.js
+++ src/store.js
@@ -1,13 +1,11 @@
 import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
 import { reducer as answersReducer } from './modules/answers';
 import { reducer as questionsReducer, questionsMiddleware } from './modules/questions';
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 export default () => {
 	return createStore(
 		combineReducers({ userAnswers: answersReducer, questions: questionsReducer }),
-		compose(
-			applyMiddleware(questionsMiddleware),
-			window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-		)
+		composeEnhancers(applyMiddleware(questionsMiddleware))
 	);
 };
